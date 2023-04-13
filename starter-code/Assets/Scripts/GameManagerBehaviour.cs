d396bb3e0f6e2b99d594231f37ee8d204f800c40
Jacob Whitty (1000111482) <WHITJS1@student.op.ac.nz>
2023-04-13 18:40:01 +1200

    just about finished the core game

---
+++ starter-code/Assets/Scripts/GameManagerBehaviour.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class GameManagerBehaviour : MonoBehaviour {
+
+	public Text goldLabel;
+	// Use this for initialization
+	void Start () {
+		Gold = 1000;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	private int gold;
+	public int Gold {
+
+		get { return gold; }
+		set
+		{
+			gold = value;
+			goldLabel.GetComponent<Text>().text = "GOLD: " + gold;
+		}
+
+	}
+}
